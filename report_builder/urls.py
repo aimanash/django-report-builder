4d8486e500ddcf4f2104d8ee41f7d0d6806e6f35
aimanash <i_man_91@yahoo.com>
2025-02-25 16:08:09 +0800

    Add basename

--- report_builder/urls.py
+++ report_builder/urls.py
@@ -6,7 +6,7 @@ from .api import views as api_views
 from django.conf import settings
 
 router = routers.DefaultRouter()
-router.register(r'reports', api_views.ReportViewSet)
+router.register(r'reports', api_views.ReportViewSet, basename="reports")
 router.register(r'report', api_views.ReportNestedViewSet)
 router.register(r'formats', api_views.FormatViewSet)
 router.register(r'filterfields', api_views.FilterFieldViewSet)
