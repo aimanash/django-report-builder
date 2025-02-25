dfcac709ca1e37bb89384f4e165d6f403670a60f
aimanash <i_man_91@yahoo.com>
2025-02-25 15:02:58 +0800

    Replace from django.conf.urls import url, include with from django.conf.urls import url, include

--- report_builder_demo/urls.py
+++ report_builder_demo/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import re_path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,9 +7,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^report_builder/', include('report_builder_scheduled.urls')),
-    url(r'^report_builder/', include('report_builder.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^report_builder/', include('report_builder_scheduled.urls')),
+    re_path(r'^report_builder/', include('report_builder.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
